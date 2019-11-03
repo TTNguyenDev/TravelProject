47e7a9c1d5141a9be5deb238209dad45a7b02f94
phithang711 <pthang769@gmail.com>
2019-11-03 14:56:43 +0700

    Complete version 1 of landing page. Update Readme Update some theme for landing page

--- travel_project/src/components/Landing/index.js
+++ travel_project/src/components/Landing/index.js
@@ -7,34 +7,74 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import Background from '../Img/home-background.jpg'
+import styled from 'styled-components'
 
 import HomePage from '../Home';
 import * as ROUTES from '../../constants/routes';
 
+
+
+const Style= styled.div`
+    .Sumary{
+    }
+    .Imgbackground{
+        background-image: url(${Background});  
+        width: 100%;
+        height:100vh;
+
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        
+        /* Needed to position the navbar */
+        position: relative;
+    }
+    .Homebuttondesign{
+        color: red
+    }
+    .NavLogo{
+        font-size: 40px
+        background-color: rgb(0,0,255)
+    }
+    .Navcolor{
+        font-size: 20px
+        background-color: rgb(0,144,255)
+    }
+`;
+
 const Landing = () => (
     <div>
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href={ROUTES.HOME} component={HomePage}><h2>HOME</h2></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-        <NavDropdown title="Logo" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Ẩm thực</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.2">Du lịch</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.3">Khuyến mãi</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Tin Tức</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-    </Navbar.Collapse>
-    </Navbar>
-    <img src={Background} style={{ width: '100%', aspectRatio: 1 }}></img>
+    
+    <Style>
+        <div className="Imgbackground">
+        <div className="Sumary">
+            <Navbar expand="lg">
+            <Navbar.Brand href={ROUTES.HOME} component={HomePage} className="Homebuttondesign"><h2>HOME</h2></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <NavDropdown title="Logo" id="basic-nav-dropdown" className="NavLogo">
+            <div className="Navcolor">
+                <NavDropdown.Item href="#action/3.1">Ẩm thực</NavDropdown.Item>
+                <NavDropdown.Divider  />
+                <NavDropdown.Item href="#action/3.2">Du lịch</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.3" >Khuyến mãi</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" >Tin Tức</NavDropdown.Item>
+            </div>
+            </NavDropdown>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+            </Navbar>
+        </div>
+        </div>
+
+    </Style>
     </div>
 );
 
