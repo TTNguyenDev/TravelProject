6548a2dd8a3516909b6b6c73e40ba793d7e734ca
TTNguyen <nttriet9a4@gmail.com>
2019-11-16 13:31:41 +0700

    Handle control session CHange Password(In progress)

--- travel_project/src/components/Navigation/index.js
+++ travel_project/src/components/Navigation/index.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import SignOut from '../SignOut';
+import { AuthUserContext } from '../Session';
 
-const Navigation = ({ authUser }) => (
-    <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
-);
+const Navigation = () => (
+    <div>
+        <AuthUserContext.Consumer>
+            {authUser => authUser ? <NavigationAuth/> : <NavigationNonAuth/>}
+        </AuthUserContext.Consumer>
+    </div>
+)
 
 const NavigationAuth = () => (
     <div>
--- travel_project/src/components/SignIn/index.js
+++ travel_project/src/components/SignIn/index.js
@@ -3,14 +3,16 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
 import { withFirebase } from '../Firebase'
 import * as ROUTES from '../../constants/routes';
 
 const SignInPage = () => (
     <div>
         <h1>SignIn</h1>
-        <SignInForm />
-        <SignUpLink />
+        <SignInForm/>
+        <PasswordForgetLink/>
+        <SignUpLink/>
     </div>
 );
 
