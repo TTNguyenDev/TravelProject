9b46c177ddaae8a2e7c760e95df5377c7724ba68
phithang711 <pthang769@gmail.com>
2019-11-17 20:21:01 +0700

    landing page fix

--- travel_project/src/components/Landing/index.js
+++ travel_project/src/components/Landing/index.js
@@ -34,11 +34,6 @@ const Style= styled.div`
     .Homebuttondesign{
         color: red
     }
-    .NavLogo{
-    }
-    .Navcolor{
-        background-color: rgb(0,0,255)
-    }
     .Blink-text{
         text-align: center;
     }
@@ -51,26 +46,15 @@ const Landing = () => (
         <div className="Imgbackground">
         <div className="Sumary">
             <Navbar expand="lg">
-            <Navbar.Brand href={ROUTES.HOME} component={HomePage} className="Homebuttondesign"><h2>HOME</h2></Navbar.Brand>
+            <Navbar.Brand href={ROUTES.HOME} component={HomePage} className="Homebuttondesign"><h2>Trang chủ</h2></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-            <NavDropdown id="basic-nav-dropdown" title={
+            <Nav.Link> 
                 <div className="NavLogo">
                     <img src={Logo} alt="Your travel" />
-                    
                 </div>
-            }>
-            <div className="Navcolor">
-                <NavDropdown.Item href="#action/3.1">Ẩm thực</NavDropdown.Item>
-                <NavDropdown.Divider  />
-                <NavDropdown.Item href="#action/3.2">Du lịch</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.3" >Khuyến mãi</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4" >Tin Tức</NavDropdown.Item>
-            </div>
-            </NavDropdown>
+            </Nav.Link>
             </Nav>
             <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -80,17 +64,16 @@ const Landing = () => (
             </Navbar>
 
             <div  className="Blink-text" >
-            <Blink color='#01FF70' text="𝓨𝓞𝓤𝓡 𝓣𝓡𝓐𝓥𝓔𝓛"  >
-                <h1></h1>
+            <Blink color='#FF851B' text="𝓨𝓞𝓤𝓡 𝓣𝓡𝓐𝓥𝓔𝓛"  >
+                    <h2></h2>
             </Blink>
-            <Blink color='#01FF70' text='𝕸𝖆𝖐𝖊 𝖞𝖔𝖚𝖗 𝖒𝖊𝖒𝖔𝖗𝖞 𝖋𝖔𝖗𝖊𝖛𝖊𝖗'>
-                <h3></h3>
+            <Blink color='#FF851B' text='Make your memory forever' >
             </Blink>
             </div>
         </div>
         </div>
-        
     </Style>
+    
     </div>
 );
 
